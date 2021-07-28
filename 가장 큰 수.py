87f30b60f4812d82cd9d42c14baae5023c95cfcd
이소정 <66251759+i-soj-ng@users.noreply.github.com>
2021-07-28 17:31:08 +0900

    프로그래머스 가장 큰 수 문제 풀이

--- 가장 큰 수.py
+++ 가장 큰 수.py
@@ -8,7 +8,6 @@ def solution(numbers):
     # numbers가 [0, 0, 0]일 때, 그냥 join해버리면 answer = "000"이 됨. 이를 해결하기 위해 조건문 사용
     if numbers[0] == '0':
         answer = "0"
-    
     else:
         answer = "".join(numbers)
     
