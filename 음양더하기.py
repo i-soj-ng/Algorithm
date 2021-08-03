250304f235b659bed3682a962b26c8bcbb2f395e
이소정 <66251759+i-soj-ng@users.noreply.github.com>
2021-08-03 20:18:33 +0900

    프로그래머스 음양더하기 문제 풀이

---
+++ 음양더하기.py
@@ -0,0 +1,12 @@
+# 문제: https://programmers.co.kr/learn/courses/30/lessons/76501
+
+def solution(absolutes, signs):
+    answer = 0
+    
+    for i in range(len(signs)):
+        if signs[i] == True:
+            answer += absolutes[i]
+        elif signs[i] == False:
+            answer -= absolutes[i]
+            
+    return answer
