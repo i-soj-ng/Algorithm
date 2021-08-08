87b4f7a358e6b1ba7dcbe12af92d30c0bc98dab0
이소정 <66251759+i-soj-ng@users.noreply.github.com>
2021-08-09 00:25:01 +0900

    프로그래머스 프린터 문제 풀이

---
+++ 프린터.py
@@ -0,0 +1,24 @@
+# 문제: https://programmers.co.kr/learn/courses/30/lessons/42587
+
+def solution(priorities, location):
+    answer = 0
+    
+    while(len(priorities) != 0):
+        if location == 0:
+            if priorities[0] < max(priorities):
+                priorities.append(priorities.pop(0))
+                location = len(priorities) - 1
+            else:
+                return answer+1
+        
+        else:
+            if priorities[0] < max(priorities):
+                priorities.append(priorities.pop(0))
+                location -= 1
+            else:
+                priorities.pop(0)
+                location -= 1
+                answer += 1
+        
+    return answer
+  
